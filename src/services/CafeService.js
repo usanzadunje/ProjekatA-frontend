a6afe5a802681162940b18bbd705a5f09b86e642
Dusan Djordjevic <dussann1997@live.com>
2021-04-09 21:03:58 +0200

    Functions now hit endpoints that return only column that are actually user

--- src/services/CafeService.js
+++ src/services/CafeService.js
@@ -5,8 +5,14 @@ export default {
     index() {
         return API.apiClient.get(`/cafes`);
     },
+    getCafeCardInfo() {
+        // Only fetching columns needed to show in cafe card component
+        // Search and Home screen have it
+        return API.apiClient.get(`/cafes?columns=cafeCardInfo`);
+    },
     show(id) {
-        return API.apiClient.get(`/cafes/${id}`);
+        // Only getting
+        return API.apiClient.get(`/cafes/${id}?columns=cafeShowInfo`);
     },
     async subscribe(cafeId) {
         await authClient.get("/sanctum/csrf-cookie");
